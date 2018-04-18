70980240ca56fb88f982d16a9331df591bdea72d
monochromePinguin <jaimaille.com@gmail.com>
2018-04-18 11:58:01 +0200

    Deletion of unneeded experimental files

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,26 +10,6 @@ use Model\ConcertManager;
  */
 class UserController extends AbstractController
 {
-
-    public function testList()
-    {
-        $concertManager = new ConcertManager($this->errorStore);
-        $concerts = $concertManager->selectAll();
-
-        return $this->twig->render(
-            'User/testList.html.twig',
-            [
-                'concerts' => $concerts,
-                'errorList' => $this->errorStore ?
-                    $this->errorStore->getAllMsg() : null
-            ]
-        );
-    }
-
-
-    /**
-    * TEST : display items – no editing for now
-    */
     public function concerts()
     {
         $concertManager = new ConcertManager($this->errorStore);
